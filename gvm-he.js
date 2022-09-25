b916f1baae1e25789739f586fe174a5b3e08ae72
TajGVM <taj@greenvision.media>
2022-09-25 16:26:05 -0600

    Use both data attributes + id

--- gvm-he.js
+++ gvm-he.js
@@ -1,5 +1,9 @@
-let email = document.getElementById('gvm-he');
-let user = email.dataset.user;
-let site = email.dataset.site;
-email.innerHTML(user + '@' + site);
-email.setAttribute('href', 'mailto:' + user + '@' + site);
+let emails = document.querySelectorAll('a[data-user]');
+emails.forEach((e) => {
+    let user = e.dataset.user;
+    let site = e.dataset.site; 
+    e.setAttribute('href', 'mailto:' + user + '@' + site);
+    if (e.id=='gvm-he') {
+        e.innerHTML(user + '@' + site);
+    }
+});
