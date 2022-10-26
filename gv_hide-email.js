59b41a53f678ece228a476248787303182805ecb
Taj DeHart <taj@greenvision.media>
2022-10-26 08:21:59 -0600

    Changed name, now using data-attribute for options

--- gv_hide-email.js
+++ gv_hide-email.js
@@ -1,9 +1,9 @@
-let emails = document.querySelectorAll('a[data-user]');
+let emails = document.getElementsByClassName('gv_hide-email') //doesnt get the internal email itself;
 emails.forEach((e) => {
     let user = e.dataset.user;
     let site = e.dataset.site; 
     e.setAttribute('href', 'mailto:' + user + '@' + site);
-    if (e.innerHTML == 'gvm-he') {
+    if (e.dataset.replace) {
         e.innerHTML = user + '@' + site;
     }
 });
