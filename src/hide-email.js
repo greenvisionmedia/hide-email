5389a242ef5e5f086fa443e6be3c574f51e114b7
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-14 11:23:55 -0700

    fixed query and keep-text bool

--- src/hide-email.js
+++ src/hide-email.js
@@ -1,4 +1,11 @@
-let emails = document.querySelectorAll('gv_hide-email');
+/**
+ * gv_hide-email
+ * Add a client's email to the html attributes of an inline element; the email will appear instead of whatever content you place in the element
+ */
+
+let emails = document.querySelectorAll('.gv_hide-email');
+
+//The first data-user and data-site you set will populate the rest of the emails on the page, unless they're overriden
 let user = emails[0].dataset.user;
 let site = emails[0].dataset.site;
 emails.forEach((e) => {
@@ -10,7 +17,7 @@ emails.forEach((e) => {
     } else {
         e.setAttribute('href', 'mailto:' + user + '@' + site);
     }
-    if (!e.dataset.keepText) {
+    if (!e.hasAttribute('data-keep-text')) {
         e.innerHTML = user + '@' + site;
     }
 });
