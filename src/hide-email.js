025e58cbbe0d7c81c2420fdefc79fff6958db07f
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-16 14:30:51 -0700

    gv-

--- src/hide-email.js
+++ src/hide-email.js
@@ -1,23 +1,26 @@
 /**
- * gv_hide-email
+ * gv-hide-email
  * Add a client's email to the html attributes of an inline element; the email will appear instead of whatever content you place in the element
  */
+{
+    let emails = document.querySelectorAll('.gv-hide-email');
 
-let emails = document.querySelectorAll('.gv_hide-email');
+    //The first data-user and data-site you set will populate the rest of the emails on the page, unless they're overriden
 
-//The first data-user and data-site you set will populate the rest of the emails on the page, unless they're overriden
-let user = emails[0].dataset.user;
-let site = emails[0].dataset.site;
-emails.forEach((e) => {
-    if (e.dataset.site) {
-        e.setAttribute(
-            'href',
-            'mailto:' + e.dataset.user + '@' + e.dataset.site
-        );
-    } else {
-        e.setAttribute('href', 'mailto:' + user + '@' + site);
-    }
-    if (!e.hasAttribute('data-keep-text')) {
-        e.innerHTML = user + '@' + site;
-    }
-});
+    let user = emails[0].dataset.user;
+    let site = emails[0].dataset.site;
+
+    emails.forEach((e) => {
+        if (e.dataset.site) {
+            e.setAttribute(
+                'href',
+                'mailto:' + e.dataset.user + '@' + e.dataset.site
+            );
+        } else {
+            e.setAttribute('href', 'mailto:' + user + '@' + site);
+        }
+        if (!e.hasAttribute('data-keep-text')) {
+            e.innerHTML = user + '@' + site;
+        }
+    });
+}
